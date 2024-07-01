72bae99aa9754eb0161a4c044ea7101f359c86df
Ken Taylor <ken@vikingsasquatch.com>
2024-06-30 20:35:23 -0400

    update settings and utils

---
+++ src/module/helpers/settings.ts
@@ -0,0 +1,50 @@
+import { TLGCC } from './constants';
+import { Logger, LogLevel} from '../utils/logger';
+
+const logger = Logger.getInstance();
+
+class Settings {
+
+  static registerSettings(): void {
+    // Add a log level setting to the system settings
+    // @ts-ignore
+    game.settings.register(TLGCC.SYSTEM_ID, 'logLevel', {
+      name: 'Logging Level',
+      hint: 'Set the level of logging for the system in Developer Tools (F12)',
+      scope: 'world',
+      config: true,
+      type: String,
+      choices: {
+        'debug': 'Debug',
+        'info': 'Info',
+        'warn': 'Warning',
+        'error': 'Error'
+      },
+      default: 'info',
+      onChange: value => {
+        const level = LogLevel[value.toUpperCase() as keyof typeof LogLevel];
+        logger.setLogLevel(level);
+      }
+    });
+
+
+    // game.settings.register(MODULE_ID, 'exampleSetting', {
+    //   name: 'Example Setting',
+    //   hint: 'This is an example setting.',
+    //   scope: 'world',
+    //   config: true,
+    //   default: true,
+    //   type: Boolean,
+    //   onChange: (value) => {
+    //     console.log(`Example setting changed to ${value}`);
+    //   },
+    // });
+  }
+
+  static get logLevel(): string {
+    // @ts-ignore
+    return game.settings.get(TLGCC.SYSTEM_ID, 'logLevel');
+  }
+}
+
+export default Settings;
---
+++ src/module/utils/utils.ts
@@ -0,0 +1,136 @@
+import { Logger } from './logger';
+
+const logger = Logger.getInstance();
+
+/* Copyright notice and license */
+// ... (Keep the existing copyright notice)
+
+/* -------------------------------------------- */
+/*  Object Helpers                              */
+/* -------------------------------------------- */
+
+/**
+ * Sort the provided object by its values or by an inner sortKey.
+ * @param obj The object to sort.
+ * @param sortKey An inner key upon which to sort.
+ * @returns A copy of the original object that has been sorted.
+ */
+export function sortObjectEntries<T extends Record<string, any>>(obj: T, sortKey?: keyof T[keyof T]): T {
+  const sorted = Object.entries(obj).sort(([, a], [, b]) => {
+    if (sortKey) {
+      return (a[sortKey] as string).localeCompare(b[sortKey] as string);
+    }
+    return (a as string).localeCompare(b as string);
+  });
+  return Object.fromEntries(sorted) as T;
+}
+
+/* -------------------------------------------- */
+
+/**
+ * Retrieve the indexed data for a Document using its UUID. Will never return a result for embedded documents.
+ * @param uuid The UUID of the Document index to retrieve.
+ * @returns Document's index if one could be found.
+ */
+export function indexFromUuid(uuid: string): object | null {
+  const parts = uuid.split('.');
+  let index: object | undefined;
+
+  if (parts[0] === 'Compendium') {
+    const [, scope, packName, id] = parts;
+    // @ts-ignore
+    const pack = game.packs.get(`${scope}.${packName}`);
+    index = pack?.index.get(id);
+  } else if (parts.length < 3) {
+    const [docName, id] = parts;
+    const collection = CONFIG[docName].collection.instance;
+    index = collection.get(id);
+  }
+
+  return index ?? null;
+}
+
+/* -------------------------------------------- */
+
+/**
+ * Creates an HTML document link for the provided UUID.
+ * @param uuid UUID for which to produce the link.
+ * @returns Link to the item or empty string if item wasn't found.
+ */
+export function createUuidLink(uuid: string): string {
+  // @ts-ignore
+  const index = game.tlgcc.utils.indexFromUuid(uuid);
+  // @ts-ignore
+  const linkText = index ? index.name : game.i18n.localize('TLGCC.Unknown');
+  const link = `@UUID[${uuid}]{${linkText}}`;
+  // @ts-ignore
+  return TextEditor.enrichHTML(link, { async: false, relativeTo: null });
+}
+
+/* -------------------------------------------- */
+/*  Config Pre-Localization                     */
+/* -------------------------------------------- */
+
+interface PreLocalizationOptions {
+  key?: string;
+  keys?: string[];
+  sort?: boolean;
+}
+
+interface PreLocalizationRegistration {
+  keys: string[];
+  sort: boolean;
+}
+
+const _preLocalizationRegistrations: Record<string, PreLocalizationRegistration> = {};
+
+/**
+ * Mark the provided config key to be pre-localized during the init stage.
+ * @param configKey Key within `CONFIG.DND5E` to localize.
+ * @param options Options for pre-localization.
+ */
+export function preLocalize(configKey: string, options: PreLocalizationOptions = {}): void {
+  const { key, keys = [], sort = false } = options;
+  const finalKeys = key ? [key, ...keys] : keys;
+  _preLocalizationRegistrations[configKey] = { keys: finalKeys, sort };
+}
+
+/**
+ * Execute previously defined pre-localization tasks on the provided config object.
+ * @param config The `CONFIG.DND5E` object to localize and sort. *Will be mutated.*
+ */
+export function performPreLocalization(config: Record<string, any>): void {
+  for (const [key, settings] of Object.entries(_preLocalizationRegistrations)) {
+    _localizeObject(config[key], settings.keys);
+    if (settings.sort) {
+      config[key] = sortObjectEntries(config[key], settings.keys[0]);
+    }
+  }
+}
+
+/**
+ * Localize the values of a configuration object by translating them in-place.
+ * @param obj The configuration object to localize.
+ * @param keys List of inner keys that should be localized if this is an object.
+ */
+function _localizeObject(obj: Record<string, any>, keys: string[]): void {
+  for (const [k, v] of Object.entries(obj)) {
+    if (typeof v === 'string') {
+      // @ts-ignore
+      obj[k] = game.i18n.localize(v);
+    } else if (typeof v === 'object' && v !== null) {
+      if (!keys.length) {
+        logger.error('Localization keys must be provided for pre-localizing when target is an object.');
+        continue;
+      }
+      for (const key of keys) {
+        if (v[key]) {
+          // @ts-ignore
+          v[key] = game.i18n.localize(v[key]);
+        }
+      }
+    } else {
+      logger.error(`Pre-localized configuration values must be a string or object, ${typeof v} found for "${k}" instead.`);
+    }
+  }
+}
